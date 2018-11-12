4ffe0ae13d848a53467777c5ff52a3d693384a47
kin <kin.poon@concentra.co.uk>
2018-11-12 14:42:00 +0000

    Q1+Q2

--- src/common/Functions.js
+++ src/common/Functions.js
@@ -2,11 +2,14 @@
 
 // Converts camel case string to a sentence case
 export const formatCamelCaseString = text => {
-  return text;
+  var result = text.replace( /([A-Z])/g, " $1" )
+  return  result.charAt(0).toUpperCase() + result.slice(1)
 };
 
 // Handles page navigation
-export const handleOnPageNavigation = item =>
+//Solution for EX1: fixing the lambda function which cannot be binded with `this`. 
+export function handleOnPageNavigation(item){
   this.setState({
     page: item.key
-  });
+  })
+};
